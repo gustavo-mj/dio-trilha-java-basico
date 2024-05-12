b6e679e71b3e1945dcaed77ababc105134990774
Gustavo Monteiro Jorge <gustavomonteirojorge@gmail.com>
2024-05-12 19:19:08 -0300

    commit da aula1

--- hello-world/src/App.java
+++ hello-world/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        String nome = "Gustavo";
+        String sobrenome = "Monteiro";
+        String nomeCompleto = nomeCompleto(nome, sobrenome);
+        
+        System.out.println(nomeCompleto);
+
+    }
+    public static String nomeCompleto(String nome, String sobrenome) {
+        return nome.concat(" ").concat(sobrenome);
     }
 }
